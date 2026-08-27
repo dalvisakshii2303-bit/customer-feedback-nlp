0159d9e7c4eca7895fc4d2dfda7b186a6bbc4300
dalvisakshii2303-bit <dalvisakshii2303@gmail.com>
2026-08-27 17:35:23 +0530

    Fix NLTK resources for Streamlit deployment

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import joblib
 import re
+import nltk
 
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download("omw-1.4")
 # --------------------------------------------------
 # PAGE CONFIGURATION
 # --------------------------------------------------
